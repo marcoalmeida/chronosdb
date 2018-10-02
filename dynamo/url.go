f8ee28f38c9f2d30b09685971052268ea62153a1
Marco Almeida <marcoafalmeida@gmail.com>
2018-10-02 00:52:53 -0700

    Replace hardcoded strings with constants

--- dynamo/url.go
+++ dynamo/url.go
@@ -7,9 +7,17 @@ import (
 	"github.com/marcoalmeida/chronosdb/coretypes"
 )
 
+// query string parameters used by ChronosDB (appended to whatever InfluxDB uses for internal use)
+const (
+	qsForward  = "xdbForward"
+	qsHandoff  = "xdbHandoff"
+	qsTransfer = "xdbTransfer"
+	qsKey      = "xdbKey"
+)
+
 // return true iff the current node is coordinating this request, i.e., the `forward` parameter has not been set
 func (dyn *Dynamo) nodeIsCoordinator(form url.Values) bool {
-	return form.Get("forward") == ""
+	return form.Get(qsForward) == ""
 }
 
 // generate a URL to be used for forwarding a request
@@ -19,7 +27,7 @@ func (dyn *Dynamo) createForwardURL(node string, uri string) string {
 		return ""
 	}
 	q := u.Query()
-	q.Set("forward", "false")
+	q.Set(qsForward, "false")
 	u.RawQuery = q.Encode()
 
 	return u.String()
@@ -32,7 +40,7 @@ func (dyn *Dynamo) setHintedHandoffURL(uri string) string {
 	}
 
 	q := u.Query()
-	q.Set("hint", "true")
+	q.Set(qsHandoff, "true")
 	u.RawQuery = q.Encode()
 
 	return u.String()
@@ -40,7 +48,7 @@ func (dyn *Dynamo) setHintedHandoffURL(uri string) string {
 
 // return true iff the current request is a hint being handed off
 func (dyn *Dynamo) requestIsHintedHandoff(form url.Values) bool {
-	return form.Get("hint") == "true"
+	return form.Get(qsHandoff) == "true"
 
 }
 
@@ -49,12 +57,12 @@ func (dyn *Dynamo) requestIsHintedHandoff(form url.Values) bool {
 // one day we may have our own interface and a layer for compatibility with InfluxDB and this will be the right place
 // to create this type of URL
 func (dyn *Dynamo) createKeyTransferURL(key *coretypes.Key) string {
-	return fmt.Sprintf("/write?db=%s&key=%s&transfer=true", key.DB, key.String())
+	return fmt.Sprintf("/write?db=%s&%s=%s&%s=true", key.DB, qsKey, key.String(), qsTransfer)
 }
 
 // return true iff the current request is part of a key transfer
 func (dyn *Dynamo) requestIsKeyTransfer(form url.Values) bool {
-	return form.Get("transfer") == "true"
+	return form.Get(qsTransfer) == "true"
 }
 
 func (dyn *Dynamo) getKeyFromURL(form url.Values) *coretypes.Key {
